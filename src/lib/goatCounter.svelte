a61af871e48f6470dd80694c76ab83137a430e26
Johnny Dean <johndevince@mail.fresnostate.edu>
2022-08-21 18:48:43 -0700

    goatcounter

---
+++ src/lib/goatCounter.svelte
@@ -0,0 +1,5 @@
+<script
+	data-goatcounter="https://johnnydean.goatcounter.com/count"
+	async
+	src="//gc.zgo.at/count.js"
+></script>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
 	import '../app.css';
+	import Anayltics from '../lib/goatCounter.svelte';
 </script>
 
+<svelte:head>
+	<script
+		data-goatcounter="https://johnnydean.goatcounter.com/count"
+		async
+		src="//gc.zgo.at/count.js"></script>
+</svelte:head>
 <slot />
