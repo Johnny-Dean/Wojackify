4d16465f550fc05160d30ef0c7a2998e8433249c
Johnny Dean <johndevince@mail.fresnostate.edu>
2022-09-06 19:18:10 -0700

    Fix navbar

--- src/components/nav/nav.svelte
+++ src/components/nav/nav.svelte
@@ -2,7 +2,6 @@
 	<div class="flex-1 ml-4">
 		<h1 class="text-2xl font-bold"><a href="/">Wojackify</a></h1>
 	</div>
-	Contact: wojackify@gmail.com
 	<div class="flex-none">
 		<ul class="menu menu-horizontal">
 			<li><a class="text-xl" href="/faq">FAQ</a></li>
